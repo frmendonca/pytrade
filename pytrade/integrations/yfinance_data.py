6c976e31a604b02a0a91d431f6cea1b1c04c97b2
frmendonca <francisco.tmendonca@hotmail.com>
2025-04-06 18:59:06 +0100

    portfolio weighting

--- pytrade/integrations/yfinance_data.py
+++ pytrade/integrations/yfinance_data.py
@@ -2,31 +2,19 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
-
 
 class YFinanceClient:
-    def __init__(self, symbols: list[str]) -> None:
-        self.symbols = symbols
 
     @staticmethod
-    def _fetch_data(symbol) -> tuple[str, pd.DataFrame]:
-        data = yf.download(symbol)
-        data.columns = data.columns.get_level_values(level=0)
-        data = data.assign(returns=lambda _df: np.log(_df["Close"]).diff(1)).dropna(
-            how="any", axis=0
-        )
+    def fetch_data(symbols: list[str]) -> tuple[str, pd.DataFrame]:
+        data = yf.download(symbols)
 
-        return symbol, data
+        relevant_cols = [("Close", symbol) for symbol in symbols]
+        data = data[relevant_cols].copy()
+        data.columns = data.columns.get_level_values(level=1)
 
-    def fetch_data(self) -> dict[str, pd.DataFrame]:
-        results = {}
-        with ThreadPoolExecutor(max_workers=8) as executor:
-            futures = {
-                executor.submit(self._fetch_data, symbol) for symbol in self.symbols
-            }
-            for future in as_completed(futures):
-                symbol, data = future.result()
-                results[symbol] = data
+        # Compute returns
+        data = np.log(data).diff(1)
+        data = data.dropna(how="any", axis=0)
 
-        return results
+        return data
