156bdf406718df6f60cc25cc9454f7420d340538
frmendonca <francisco.tmendonca@hotmail.com>
2025-05-14 22:00:03 +0100

    changes to simulation strategy

--- pytrade/integrations/yfinance_data.py
+++ pytrade/integrations/yfinance_data.py
@@ -6,15 +6,17 @@ import numpy as np
 class YFinanceClient:
 
     @staticmethod
-    def fetch_data(symbols: list[str]) -> tuple[str, pd.DataFrame]:
+    def fetch_data(symbols: list[str]) -> tuple[pd.Series, pd.DataFrame]:
         data = yf.download(symbols)
 
         relevant_cols = [("Close", symbol) for symbol in symbols]
         data = data[relevant_cols].copy()
         data.columns = data.columns.get_level_values(level=1)
 
+        last_prices = data.iloc[-1]
+
         # Compute returns
         data = np.log(data).diff(1)
         data = data.dropna(how="any", axis=0)
 
-        return data
+        return last_prices[symbols], data[symbols] # Enforce same order as passed list
--- pytrade/simulation/simulation.py
+++ pytrade/simulation/simulation.py
@@ -1,8 +1,9 @@
-import typing as t
+
 import numpy as np
-from scipy.stats._distn_infrastructure import rv_continuous_frozen as RVContinuousFrozen
+import pandas as pd
+
 from pytrade.data_models.base import SimulationConfig, SimulationResults
-from pytrade.data_models.option import Option, OptionDirection
+from pytrade.data_models.option import Option, OptionDirection, OptionType
 
 
 def compute_hedge_cost(options: list[Option]) -> float:
@@ -16,8 +17,8 @@ def compute_hedge_cost(options: list[Option]) -> float:
     debit = sum(
         [
             (
-                100 * option._premium * option._contracts
-                if option._option_direction == OptionDirection.LONG
+                100 * option.premium * option.contracts
+                if option.option_direction == OptionDirection.LONG
                 else 0
             )
             for option in options
@@ -27,8 +28,8 @@ def compute_hedge_cost(options: list[Option]) -> float:
     credit = sum(
         [
             (
-                -100 * option._premium * option._contracts
-                if option._option_direction == OptionDirection.SHORT
+                -100 * option.premium * option.contracts
+                if option.option_direction == OptionDirection.SHORT
                 else 0
             )
             for option in options
@@ -38,7 +39,7 @@ def compute_hedge_cost(options: list[Option]) -> float:
     return debit + credit
 
 
-def compute_intrinsic_hedge_value(options: list[Option], underlying: float):
+def compute_intrinsic_hedge_value(options: list[Option], underlying: float) -> float:
     """
     Computes the value of an hedge using intrinsic value
     and assuming oposite direction to that of the Option.
@@ -49,13 +50,14 @@ def compute_intrinsic_hedge_value(options: list[Option], underlying: float):
     at expiration.
 
     :param options a list of Option ojects
+    :param underlying a float with the current underlying price
     :returns float value of the hedge
     """
     sell_to_close = sum(
         [
             (
-                100 * option.compute_intrinsic_value(underlying)
-                if option._option_direction == OptionDirection.LONG
+                100 * option.compute_intrinsic_value(underlying) * option.contracts
+                if option.option_direction == OptionDirection.LONG
                 else 0
             )
             for option in options
@@ -65,8 +67,8 @@ def compute_intrinsic_hedge_value(options: list[Option], underlying: float):
     buy_to_close = sum(
         [
             (
-                -100 * option.compute_intrinsic_value(underlying)
-                if option._option_direction == OptionDirection.SHORT
+                -100 * option.compute_intrinsic_value(underlying) * option.contracts
+                if option.option_direction == OptionDirection.SHORT
                 else 0
             )
             for option in options
@@ -79,17 +81,20 @@ def compute_intrinsic_hedge_value(options: list[Option], underlying: float):
 def compute_bs_hedge_value(
     options: list[Option],
     underlying: float,
-    iv_change: float | None = None,
-    dte_change: int | None = None,
+    iv_change: float = 0.0,
+    dte_change: int = 0.0,
 ):
     """
-    Computes the value of an hedge using black scholes
+    Computes the value of a hedge using black scholes
     and assuming oposite direction to that of the Option.
     For a SHORT call it assumes it will be bought to close
     resulting in a debit and for a LONG call it assumes
     it will be sold to close, resulting in a credit.
 
     :param options a list of Option ojects
+    :param underlying a float with the current underlying asset price
+    :param iv_change Optional float with the change in implied volatily. If null use Option object IV
+    :param dte_change Optional int with the change in days_to_expiry to use in the computation. If null uses Option object dte
     :returns float value of the hedge
     """
 
@@ -99,10 +104,10 @@ def compute_bs_hedge_value(
                 100
                 * option.compute_black_scholes_value(
                     underlying,
-                    max(0.08, option._iv * (1 + iv_change)),
-                    option._days_to_expiration - dte_change,
-                )
-                if option._option_direction == OptionDirection.LONG
+                    option.iv + iv_change,
+                    option.days_to_expiration - dte_change,
+                ) * option.contracts
+                if option.option_direction == OptionDirection.LONG
                 else 0
             )
             for option in options
@@ -115,10 +120,10 @@ def compute_bs_hedge_value(
                 -100
                 * option.compute_black_scholes_value(
                     underlying,
-                    max(0.08, option._iv * (1 + iv_change)),
-                    option._days_to_expiration - dte_change,
-                )
-                if option._option_direction == OptionDirection.SHORT
+                    option.iv + iv_change,
+                    option.days_to_expiration - dte_change,
+                ) * option.contracts
+                if option.option_direction == OptionDirection.SHORT
                 else 0
             )
             for option in options
@@ -130,56 +135,70 @@ def compute_bs_hedge_value(
 
 def simulate_returns(
     *,
-    generator: RVContinuousFrozen,
+    returns: pd.Series,
     initial_underlying: float,
     options: list[Option],
     simulation_config: SimulationConfig,
-    volatility_generator: RVContinuousFrozen | None = None,
     compute_at_intrinsic: bool = True,
-    nsim: int = 5000
 ) -> SimulationResults:
+    """
+    Transforms a sequence of returns into a sequence
+    of returns coupled with an option strategy.
 
-    hedge_cost = compute_hedge_cost(options)
+    The transformation occurs by assuming a one step process where
+    it is assumed that an option(s) is bought or sold at T0 and evaluated
+    at T1. The original returns from T0 to T1 are then adjusted reflect the
+    change in the option strategy
+
+    :param returns a pandas series containing original returns
+    :param initial_underlying a float with the current underlying price
+    :param options a list of Option objects with the option strategy
+    :param simulation_config a SimulationConfig object with the configuration for the simulations
+    :param compute_at_intrinsic boolean equal to True if options should be evaluated at expiration
+    :returns SimulationResults
+    """
+
+    if not isinstance(returns, np.ndarray):
+        returns = np.array(returns)
+
+    strategy_cost = compute_hedge_cost(options)
     invested = simulation_config.portfolio_value + (
-        simulation_config.monthly_contributions - hedge_cost
+        simulation_config.monthly_contributions - strategy_cost
     )
 
-    # Draw returns from generator and project next period portfolio
-    returns = generator.rvs(size=nsim)
+    # Change in underlying price from T0 to T1
     underlying_next = initial_underlying * (1 + returns)
     invested_next = invested * (1 + returns)
 
-    # Compute hedge value
+    # Compute strategy value
     if compute_at_intrinsic:
-        vix_returns = np.nan
-        hedge_value = np.array(
+        strategy_value = np.array(
             [
                 compute_intrinsic_hedge_value(options, underlying)
                 for underlying in underlying_next
             ]
         )
     else:
-        vix_returns = volatility_generator.rvs(returns)
-        hedge_value = np.array(
+        strategy_value = np.array(
             [
                 compute_bs_hedge_value(
                     options,
-                    underlying_next[i],
-                    vix_returns[i],
-                    simulation_config.returns_frequency,
+                    undnext,
+                    0, #TODO: Add change in IV according to market return
+                    simulation_config.option_dte_change,
                 )
-                for i in range(len(underlying_next))
+                for undnext in underlying_next
             ]
         )
 
     # Compute total return
-    final_portfolio = invested_next + hedge_value
-    hedged_returns = (
+    final_portfolio = invested_next + strategy_value
+    strategy_returns = (
         final_portfolio
         / (simulation_config.portfolio_value + simulation_config.monthly_contributions)
         - 1
     )
 
     return SimulationResults(
-        hedged_returns=hedged_returns, unhedged_returns=returns, vix_returns=vix_returns
+        transformed_returns=strategy_returns, original_returns=returns
     )
